52061903961ac8c6b760f3073bc04077526ca587
Jimmy <jimmy.papandrea@vtsd.com>
2024-03-06 12:23:32 -0500

    3.1.3

---
+++ 3.1.3/step3.py
@@ -0,0 +1,15 @@
+sandwich_type = ""
+sandwich_price = ""
+
+print("what type of sandwich would you like?")
+print("chicken $5.25, beef $6.25, tofu $5.75")
+sandwich_price == imput
+sandwich_type == "chicken":
+print("")
+
+
+
+
+
+
+
