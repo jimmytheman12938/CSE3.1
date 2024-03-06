52d958d98c750154470e232cf462296f59c97565
Jimmy <jimmy.papandrea@vtsd.com>
2024-03-06 12:35:16 -0500

    3.1.3

--- 3.1.3/step3.py
+++ 3.1.3/step3.py
@@ -1,11 +1,22 @@
 sandwich_type = ""
-sandwich_price = ""
+subtotal = 0.0
 
-print("what type of sandwich would you like?")
+# Explaining Menu Options
+print("What type of sandwich would you like?")
 print("chicken $5.25, beef $6.25, tofu $5.75")
-sandwich_price == imput
-sandwich_type == "chicken":
-print("")
+sandwich_type = input("Sandwich Choice: ")
+
+# Calculating Sandwich Price
+if sandwich_type == "chicken":
+    print("You chose chicken, which will be $5.25")
+    subtotal += 5.25
+elif sandwich_type == "tofu":
+    print("You chose tofu, which will be $5.75")
+    subtotal += 5.75
+else:
+    print("You chose beef, which will be $6.25")
+    subtotal += 6.25
+
 
 
 
