cbeef06bb5745137622098c44e837d441e79cfbf
Jimmy <jimmy.papandrea@vtsd.com>
2024-03-07 12:22:45 -0500

    3.1.3

--- 3.1.3/step3.py
+++ 3.1.3/step3.py
@@ -17,6 +17,46 @@ else:
     print("You chose beef, which will be $6.25")
     subtotal += 6.25
 
+beverage_type = ""
+subtotal = 0.0
+
+print("What size of drink would you like?")
+print("A small drink is $1.00, a medium drink is $1.75, and a large drink is $2.25")
+beverage_type = input("beverage choice:")
+
+if beverage == "small drink"
+    print("a small drink will be $1.00")
+    subtotal += 1.00
+
+elif beverage == "medium drink"
+    print("a medium drink will be $1.75")
+    subtotal += 1.75
+
+else:
+    print("a large drink will be $2.25")
+    subtotal += 2.25
+
+side_type = ""
+subtotal = 0.0
+
+print("What type of side would you like?")
+print("A side of fries is $1.00, a side of onion rings is $2.00, and a side salad is $2.50")
+side_type = input("side choice:")
+
+if side == "side of fires"
+    print("a side of fries will be $1.00")
+    subtotal += 1.00
+
+elif side == "side of onion rings"
+    print("a side of onion rings will be $2.00")
+    subtotal += 2.00
+
+else:
+    print("a side salad will be $2.50")
+    subtotal += 2.50
+
+
+
 
 
 
