14386a1b6f9799844294cc28ca7e7ae892d8900a
Jimmy <jimmy.papandrea@vtsd.com>
2024-03-15 11:44:11 -0400

    3.1.3

--- 3.2.1/combomenu.py
+++ 3.2.1/combomenu.py
@@ -1,6 +1,8 @@
 sandwich_type = ""
 subtotal = 0.0
 
+order_list = []
+
 # Explaining Menu Options
 print("What type of sandwich would you like?")
 print("chicken $5.25, beef $6.25, tofu $5.75")
@@ -10,12 +12,15 @@ sandwich_type = input("Sandwich Choice: ")
 if sandwich_type == "chicken":
     print("You chose chicken, which will be $5.25")
     subtotal += 5.25
+    order_list.append ("chicken")
 elif sandwich_type == "tofu":
     print("You chose tofu, which will be $5.75")
     subtotal += 5.75
+    order_list.append ("tofu")
 else:
     print("You chose beef, which will be $6.25")
     subtotal += 6.25
+    order_list.append ("beef")
 
 # Beverage Choice
 bev_choice = input("Do you want a beverage? yes or no")
@@ -24,12 +29,15 @@ if bev_choice == "yes":
     if bev_size == "small":
         print("You chose small, so $1.00")
         subtotal += 1
+        order_list.append("small")
     if bev_size == "medium":
         print("You chose medium, so $1.75")
         subtotal += 1.75
+        order_list.append("medium")
     if bev_size == "large":
         print("You chose large, so $2.25")
         subtotal += 2.25
+        order_list.append("large")
 
 # Fries
 fry_choice = input("Do you want french fries? Yes or No")
@@ -40,20 +48,25 @@ if fry_choice == "yes":
         if supersize == "yes":
             fry_size = "large"
             subtotal += 2
+            order_list.append("Large")
         else:
             print("You chose small fries for $1")
             subtotal += 1
+            order_list.append("Small")
     elif fry_size == "medium":
         print("You chose medium for $1.50")
         subtotal += 1.50
+        order_list.append("Medium")
     else:
         print("You chose large fries for $2")
         subtotal += 2
+        order_list.append("Large")
 
-# Kethcup
-ketchup = float(input("HJow many ketup packets would you like? $0.25 each"))
+# Ketchup
+ketchup = float(input("How many ketchup packets would you like? $0.25 each"))
 if ketchup >= 0:
     subtotal += ketchup * .25
+    order_list.append(ketchup)
 
 if bev_choice == "yes" and fry_choice == "yes":
     subtotal -= 1
@@ -64,6 +77,6 @@ if bev_choice == "yes":
 if fry_choice == "yes":
     print(fry_size)
 
+print("")
 print(subtotal)
-
-order_list =
+print(order_list)
