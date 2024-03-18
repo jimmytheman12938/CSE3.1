295df254e956f5cd3da2ae9012f12b0ce7184ae1
Jimmy <jimmy.papandrea@vtsd.com>
2024-03-18 13:54:18 -0400

    3.1.3

---
+++ introduction 3.2.2/introduction.py
@@ -0,0 +1,34 @@
+import random
+def random_number():
+    for x in range(12):
+        n1=random.radint(30,50)
+    print(n1)
+'''
+
+
+while x = 100
+    n1 = random.randint(30, 50)
+    print(n1)
+    x+= 1
+'''
+'''
+x = 0
+while x != 100:
+    random_number()
+    x += 1
+    '''
+
+def largerVal(num1, num2):
+    if num1 > num2:
+        return num1
+    elif num1 == num2
+        return num2
+
+
+    else:
+        return num2
+
+
+largerVal(14, 22)
+
+print(biggest_number)
