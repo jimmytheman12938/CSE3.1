efc67ef609ff18ce3420117eaaf1403eedd53d10
Jimmy <jimmy.papandrea@vtsd.com>
2024-04-26 11:53:23 -0400

    3.1.3

--- introduction 3.2.2/3.2.3/3.2.4 courses/3.2.5/team5.py
+++ introduction 3.2.2/3.2.3/3.2.4 courses/3.2.5/team5.py
@@ -1,28 +1,24 @@
-"""The function move(my_history, their_history) must return "r", "p", or "s".
+"""The function move(my_history, their_history) must return 'r', 'p', or 's'.
 my_history and their_history are strings of the same length, perhaps length zero.
 """
-strategy_name = "RockEarlyPaperEven"
+
 import random
 
+strategy_name = "Beat last move if repeating"
 
-def rock(my_history="", their_history=""):
-    return "r"
-
-
-def paper(my_history, their_history):
-    return "p"
-
-
-def random_move():
-    pick = random.choice(["r", "p", "s"])
-    return pick
-
+def beat_move(move):
+    if (move=="r"):
+        return "p"
+    if (move == "p"):
+        return "s"
+    if (move=="s"):
+        return "r"
 
 def move(my_history, their_history):
-    if (len(my_history) < 5):
-        my_move = rock()
-    elif (len(my_history) % 2 == 0):
-        my_move = paper(my_history, their_history)
-    else:
-        my_move = random_move()
-    return my_move
+    """This player always starts with rock
+    """
+    if (len(their_history) < 2):
+        return "p"
+    if (their_history[-1] == their_history[-2]):
+        return beat_move(their_history[-1])
+    return random.choice(["r", "p", "s"])
