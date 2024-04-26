c8480a7b7c8fb82045cd177982c5eb2c58f39649
Jimmy <jimmy.papandrea@vtsd.com>
2024-04-26 11:55:58 -0400

    3.1.3

--- introduction 3.2.2/3.2.3/3.2.4 courses/3.2.5/team9.py
+++ introduction 3.2.2/3.2.3/3.2.4 courses/3.2.5/team9.py
@@ -18,7 +18,7 @@ def move(my_history, their_history):
     """This player always starts with rock
     """
     if (len(their_history) < 2):
-        return "r"
+        return "p"
     if (their_history[-1] == their_history[-2]):
         return beat_move(their_history[-1])
     return random.choice(["r", "p", "s"])
